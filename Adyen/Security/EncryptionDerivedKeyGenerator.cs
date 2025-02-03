c0ebf432007e812f4a4b8b2867480bfe632803c5
Maurice van der Zwaan <mvdzwaan@wbservices.nl>
2025-02-03 08:57:14 +0100

    Cache the encryption derived key to increase performance

--- Adyen/Security/EncryptionDerivedKeyGenerator.cs
+++ Adyen/Security/EncryptionDerivedKeyGenerator.cs
@@ -8,19 +8,26 @@ namespace Adyen.Security
     {
         private const int Iterations = 4000;
         private const string Salt = "AdyenNexoV1Salt";
+        private static EncryptionDerivedKey CachedKey = null;
+
         internal EncryptionDerivedKey Generate(EncryptionCredentialDetails encryptionCredentialDetails)
         {
-            byte[] salt = Encoding.ASCII.GetBytes(Salt);
-
-            var rfc2898 = new Rfc2898DeriveBytes(encryptionCredentialDetails.Password, salt, Iterations);
-            byte[] key = rfc2898.GetBytes(80);
-
-            return new EncryptionDerivedKey
+            if (CachedKey == null)
             {
-                HmacKey = key.Slice(0, EncryptionDerivedKey.HmacKeyLength),
-                CipherKey = key.Slice(EncryptionDerivedKey.HmacKeyLength, EncryptionDerivedKey.HmacKeyLength + EncryptionDerivedKey.CipherKeyLength),
-                IV = key.Slice(EncryptionDerivedKey.HmacKeyLength + EncryptionDerivedKey.CipherKeyLength, EncryptionDerivedKey.HmacKeyLength + EncryptionDerivedKey.CipherKeyLength + EncryptionDerivedKey.IVLength)
-            };
+                byte[] salt = Encoding.ASCII.GetBytes(Salt);
+
+                var rfc2898 = new Rfc2898DeriveBytes(encryptionCredentialDetails.Password, salt, Iterations);
+                byte[] key = rfc2898.GetBytes(80);
+
+                CachedKey = new EncryptionDerivedKey
+                {
+                    HmacKey = key.Slice(0, EncryptionDerivedKey.HmacKeyLength),
+                    CipherKey = key.Slice(EncryptionDerivedKey.HmacKeyLength, EncryptionDerivedKey.HmacKeyLength + EncryptionDerivedKey.CipherKeyLength),
+                    IV = key.Slice(EncryptionDerivedKey.HmacKeyLength + EncryptionDerivedKey.CipherKeyLength, EncryptionDerivedKey.HmacKeyLength + EncryptionDerivedKey.CipherKeyLength + EncryptionDerivedKey.IVLength)
+                };
+            }
+
+            return CachedKey;
         }
     }
 }
